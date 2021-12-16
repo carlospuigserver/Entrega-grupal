67a4477def75eb92249f97be17f1399119fdeba3
carlospuigserver <cpuiggut@myuax.com>
2021-12-16 18:56:35 +0100

    subida

--- Sum Matriz.py
+++ Sum Matriz.py
@@ -4,19 +4,6 @@ import random
 import re
 import sys
 
-
-    
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    print("Escribe la dimanesión de la matriz")
-    ar_count = int(input().strip())
-    print("Escribe los números separados por espacios")
-    ar = list(map(int, input().rstrip().split()))
-
-
-
-
-
 def simpleArraySum(ar):
     suma=0
     for i in ar:
@@ -24,7 +11,15 @@ def simpleArraySum(ar):
         return suma 
     print("La suma de todos los elementos de la matriz es:",suma)
 
+              
+        
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    print("Escribe la dimanesión de la matriz")
+    ar_count = int(input().strip())
+    print("Escribe los números separados por espacios")
+    ar = list(map(int, input().rstrip().split()))
 
 result = simpleArraySum(ar)
 fptr.write(str(result) + '\n')
