8d96e9f7943bffec21c95424cd7c6a842456f502
carlospuigserver <cpuiggut@myuax.com>
2021-12-16 19:33:35 +0100

    subida

--- Sum Matriz.py
+++ Sum Matriz.py
@@ -6,7 +6,8 @@ import sys
 
 def simpleArraySum(ar):
     suma=0
-    for i in ar:
+    l=len(ar)
+    for i in range(l):
         suma=suma+i
         return suma 
     print("La suma de todos los elementos de la matriz es:",suma)
@@ -14,14 +15,14 @@ def simpleArraySum(ar):
               
         
 
-if __name__ == '__main__':
+def main():
+    os.environ['OUTPUT_PATH']='Solucion.txt'
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     print("Escribe la dimanesión de la matriz")
-    ar_count = int(input().strip())
+    ar_count= int(input().strip())
     print("Escribe los números separados por espacios")
     ar = list(map(int, input().rstrip().split()))
-
-result = simpleArraySum(ar)
-fptr.write(str(result) + '\n')
-fptr.close()
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
 
