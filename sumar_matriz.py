d4220dab2aba751079b2c060ce92905b361b04dd
maariagarrcia <mrrmn@myuax.com>
2021-12-17 20:52:10 +0100

    dimension matriz

--- sumar_matriz.py
+++ sumar_matriz.py
@@ -8,8 +8,9 @@ def simpleArraySum(ar):
     suma=0
     for number  in ar:
         suma=suma+number
-    return  suma
+    return suma
 
+n=int(input("Intrduzca una lista de dimension de la matriz: "))
 ar=list(map(int,input().rstrip().split()))
 print(ar)
 result=simpleArraySum(ar)
