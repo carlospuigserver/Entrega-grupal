7c9d0d6218ae532cdc6a761ebd56a29c4dd2fdaf
carlospuigserver <cpuiggut@myuax.com>
2021-12-17 21:28:36 +0100

    subida

--- Suma grande Matriz.py
+++ Suma grande Matriz.py
@@ -7,21 +7,13 @@ import sys
 
 def aVeryBigSum(ar):
     suma=0
-    for i in ar:
-        suma=suma+i
+    for number in ar:
+        suma=suma+number
         return suma
-    print("La suma de todos los elementos de la matriz es:",suma)
 
-def main():
-    os.environ['OUTPUT_PATH']='Solución.txt'
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    print("Escribe la dimensión de la matriz")
-    ar_count = int(input().strip())
-    print("Escribe los números separados por espacio")
-    ar = list(map(int, input().rstrip().split()))
-    result = aVeryBigSum(ar)
-    fptr.write(str(result) + '\n')
-    fptr.close()
-
-if __name__=='__main__':
-    main()
+n=int(input("Introduce una lista de las dimensiones de la matriz"))
+print("Introduzca una lista de numeros", end="")
+ar = list(map(int, input().rstrip().split()))
+print(ar)
+result=aVeryBigSum(ar)
+print("La suma tiene un valor de",result)        
