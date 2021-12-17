b6b8cb7bffc82f7cd61d22bc46af8f0f9e65540c
maariagarrcia <mrrmn@myuax.com>
2021-12-17 21:10:09 +0100

    errores

--- Escalera.py
+++ Escalera.py
@@ -5,15 +5,17 @@ def staircase(dimension):
     for fila_actual in range(1,dimension+1):
         num_espacios= dimension-(fila_actual)
         # Imprime espacios iniciales
-        for e in range(0,int(num_espacios)/2):
-            print("",end="")
+        for e in range(0,int((num_espacios)/2)):
+            print(" ",end="")
 
         # Imprime hastag
         for e in range(0, dimension-num_espacios):
             print("#",end="")
 
+
         # Pasar a la linea siguietne
         print()
+
     print()
 
 def pedir_dimension():
