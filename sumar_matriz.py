e80a95e36b7c82cbb81c7e897013bd33dff589b8
maariagarrcia <mrrmn@myuax.com>
2021-12-17 20:50:11 +0100

    codigo profe

--- sumar_matriz.py
+++ sumar_matriz.py
@@ -3,3 +3,11 @@ import os
 import random
 import re
 import sys
+
+def simpleArraySum(ar):
+
+
+ar=list(map(int,input().rstrip().split()))
+print(ar)
+result=simpleArraySum(ar)
+print("La suma vale", result)
