5cd9ba294af9d3f46c340375fcd54b78bfc4278c
carlospuigserver <cpuiggut@myuax.com>
2021-12-17 21:09:18 +0100

    Create sumar_matriz.py

---
+++ sumar_matriz.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    suma=0
+    for number  in ar:
+        suma=suma+number
+    return suma
+
+n=int(input("Intrduzca una lista de dimension de la matriz: "))
+print("Introduzca una lista de numeros", end="")
+ar=list(map(int,input().rstrip().split()))
+print(ar)
+result=simpleArraySum(ar)
+print("La suma vale", result)
