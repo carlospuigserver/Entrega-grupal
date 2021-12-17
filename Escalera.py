496ccd5ea9106e6b18c802f3ed7ed8150017a0a0
maariagarrcia <mrrmn@myuax.com>
2021-12-17 20:58:33 +0100

    funcion

--- Escalera.py
+++ Escalera.py
@@ -1,3 +1,15 @@
 n=int(input("Escribe de cusntos pisos quieres que sea la escalera   "))
-for i in range(n+1):
-    print("*"*i,end="")
+def staircase(dimension):
+    for fila_actual in range(1,dimension+1):
+        num_espacios= dimension-(fila_actual)
+        # Imprime espacios iniciales
+        for e in range(0,int(num_espacios)/2):
+            print("",end="")
+
+        # Imprime hastag
+        for e in range(0, dimension-num_espacios):
+            print("#",end="")
+
+        # Pasar a la linea siguietne
+        print()
+    print()
