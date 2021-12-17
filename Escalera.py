e4aaf6f2882b6958739598b3460349e165652570
maariagarrcia <mrrmn@myuax.com>
2021-12-17 21:03:21 +0100

    dimension

--- Escalera.py
+++ Escalera.py
@@ -1,4 +1,4 @@
-n=int(input("Escribe de cusntos pisos quieres que sea la escalera   "))
+
 def staircase(dimension):
     for fila_actual in range(1,dimension+1):
         num_espacios= dimension-(fila_actual)
@@ -13,3 +13,8 @@ def staircase(dimension):
         # Pasar a la linea siguietne
         print()
     print()
+
+def pedir_dimension():
+    dimension=int(input("Insertar dimension: "))
+    return dimension
+    
