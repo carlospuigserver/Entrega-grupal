92c6da31983bb173f6dd1659c4dfd70dc1112ec1
carlospuigserver <cpuiggut@myuax.com>
2021-12-18 11:09:04 +0100

    Relaciono cada nota con cada apartado

--- Comparar problemas.py
+++ Comparar problemas.py
@@ -7,3 +7,13 @@ l3=randint(0,10)
 c1=randint(0,10)
 c2=randint(0,10)
 c3=randint(0,10)
+
+print("Los puntos de Lucía son:")
+print("\nClaridad del problema={}".format(l1))
+print("\nOriginalidad={}".format(l2))
+print("\nDificultad={}".format(l3))
+
+print("Los puntos de Carlos son:")
+print("\nClaridad del problema={}".format(c1))
+print("\nOriginalidad={}".format(c2))
+print("\nDificultad={}".format(c3))
